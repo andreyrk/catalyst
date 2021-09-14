3117b2667b420d824e412e3b9683e9987d25dc57
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-09-14 23:58:27 +0000

    Deploy andreyrk/catalyst to github.com/andreyrk/catalyst.git:gh-pages

---
+++ 9072.28d453327b87048951fc.js
@@ -0,0 +1,1 @@
+(self.webpackChunkcatalyst=self.webpackChunkcatalyst||[]).push([[9072],{9072:(s,t,o)=>{"use strict";o.r(t),o.d(t,{ion_text:()=>a});var c=o(3150),r=o(7585),l=o(1269);const a=class{constructor(s){(0,c.r)(this,s)}render(){const s=(0,r.b)(this);return(0,c.h)(c.H,{class:(0,l.c)(this.color,{[s]:!0})},(0,c.h)("slot",null))}};a.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
